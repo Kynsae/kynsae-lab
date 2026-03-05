2b550879ac08161cd26bd46d659ae65f73709ba2
soop3ee <alexis.limacher@proton.me>
2026-03-05 14:37:02 -0500

    feat(home): add rotating-mirrors component and redesign home page

--- src/app/experiments/home/home.ts
+++ src/app/experiments/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RotatingMirrors } from '../../shared/components/rotating-mirrors/rotating-mirrors';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RotatingMirrors
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
